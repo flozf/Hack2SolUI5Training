d1c3c132cf1088a67904190321de05b6c0a01188
Tobias Oetzel <tobias.oetzel@sap.com>
2018-11-15 23:45:34 +0100

    step nr. 13

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -18,6 +18,13 @@ sap.ui.define([
 			var oLocaleData = new sap.ui.core.LocaleData(oLocale);
 			var oCurrency = new Currency(oLocaleData.mData.currencyFormat);
 			return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
+		},
+		onItemSelected: function(oEvent) {
+			var oSelectedItem = oEvent.getSource();
+			var oContext = oSelectedItem.getBindingContext("products");
+			var sPath = oContext.getPath();
+			var oProductDetailPanel = this.byId("productDetailsPanel");
+			oProductDetailPanel.bindElement({ path: sPath, model: "products" });
 		}
 	});
 });
